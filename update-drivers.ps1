e4afefc93f76117b4efd42f7bd8e5c8e9a1df2d2
Feodor Fitsner <feodor@appveyor.com>
2018-08-31 17:12:34 -0700

    Update Chrome Driver 2.39

---
+++ update-drivers.ps1
@@ -0,0 +1,16 @@
+Write-Host "Installing Chrome Selenium driver..." -ForegroundColor Cyan
+
+$destPath = 'C:\Tools\WebDriver'
+
+if(-not (Test-Path $destPath)) {
+    New-Item $destPath -ItemType directory -Force | Out-Null
+}
+
+$zipPath = "$env:TEMP\chromedriver_win32.zip"
+(New-Object Net.WebClient).DownloadFile('http://chromedriver.storage.googleapis.com/2.39/chromedriver_win32.zip', $zipPath)
+7z x $zipPath -aoa -o"$destPath"
+del $zipPath
+
+Add-Path $destPath
+
+Write-Host "Installed Chrome Selenium driver" -ForegroundColor Green
