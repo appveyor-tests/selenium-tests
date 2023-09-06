e251d494ed7679989321697dd14f53ed56b92cdd
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-09-05 22:21:53 -0700

    explicitly remove previous version

--- update-drivers.ps1
+++ update-drivers.ps1
@@ -6,6 +6,9 @@ if(-not (Test-Path $destPath)) {
     New-Item $destPath -ItemType directory -Force | Out-Null
 }
 
+
+Remove-Item -Path C:\Tools\WebDriver\chromedriver.exe -Force
+Remove-Item -Path C:\Tools\WebDriver\LICENSE.chromedriver -Force
 $zipPath = "$env:TEMP\chromedriver_win32.zip"
 (New-Object Net.WebClient).DownloadFile('https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win32/chromedriver-win32.zip', $zipPath)
 7z x $zipPath -aoa -o"$destPath"
