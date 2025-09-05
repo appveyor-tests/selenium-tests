d3af66e77273a2d8ed206db41e0b376a5c82326a
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2025-09-04 21:43:03 -0700

    Update update-drivers.ps1

--- update-drivers.ps1
+++ update-drivers.ps1
@@ -13,7 +13,7 @@ $zipPath = "$env:TEMP\chromedriver_win32.zip"
 (New-Object Net.WebClient).DownloadFile('https://storage.googleapis.com/chrome-for-testing-public/140.0.7339.81/win32/chromedriver-win32.zip', $zipPath)
 7z x $zipPath -aoa -o"$destPath"
 del $zipPath
-
+Get-ChildItem $destPath
 Add-Path $destPath
 
 Write-Host "Installed Chrome Selenium driver" -ForegroundColor Green
